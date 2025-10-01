a20c9789d750696305ea6f17e39d13e8375cec71
arkiver <arkiver@protonmail.com>
2025-10-01 18:25:59 +0200

    Rename kagisearch list.

--- other/create_github_kagisearch_kite-public.py
+++ other/create_github_kagisearch_kite-public.py
@@ -7,7 +7,7 @@ def main():
         timeout=10
     )
     assert response.status_code == 200
-    with open('900_create_github_kagisearch_kite-public.txt', 'w') as f:
+    with open('900_github_kagisearch_kite-public.txt', 'w') as f:
         for feed_url in [url for d in response.json().values() for url in d['feeds']]:
             f.write('random=RANDOM;all=1;keep_all=1;deep_extract=1;depth=1;url={}\n'.format(feed_url))
 
